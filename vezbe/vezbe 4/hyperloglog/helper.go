ba392aab3f93164fbae94aad9ffd8df9ccc81998
Tamara Ranković <rankovictamaraa@gmail.com>
2021-12-20 23:23:32 +0100

    hll helper file update

--- vezbe/vezbe 4/hyperloglog/helper.go
+++ vezbe/vezbe 4/hyperloglog/helper.go
@@ -18,18 +18,28 @@ type HLL struct {
 func (hll *HLL) Estimate() float64 {
 	sum := 0.0
 	for _, val := range hll.reg {
-		sum = sum + math.Pow(float64(-val), 2.0)
+		sum += math.Pow(math.Pow(2.0, float64(val)),-1)
 	}
 
 	alpha := 0.7213 / (1.0 + 1.079/float64(hll.m))
 	estimation := alpha * math.Pow(float64(hll.m), 2.0) / sum
 	emptyRegs := hll.emptyCount()
-	if estimation < 2.5*float64(hll.m) { // do small range correction
+	if estimation <= 2.5*float64(hll.m) { // do small range correction
 		if emptyRegs > 0 {
 			estimation = float64(hll.m) * math.Log(float64(hll.m)/float64(emptyRegs))
 		}
-	} else if estimation > math.Pow(2.0, 32.0)/30.0 { // do large range correction
+	} else if estimation > 1/30.0*math.Pow(2.0, 32.0) { // do large range correction
 		estimation = -math.Pow(2.0, 32.0) * math.Log(1.0-estimation/math.Pow(2.0, 32.0))
 	}
 	return estimation
+}
+
+func (hll *HLL) emptyCount() int {
+	sum := 0
+	for _, val := range hll.reg {
+		if val == 0 {
+			sum++
+		}
+	}
+	return sum
 }
